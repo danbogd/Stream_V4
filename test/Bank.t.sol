6c6665e8d07d2d9183a00f8ea8ba18823e92f4bf
Home <home@mail.com>
2023-10-11 18:11:11 +0300

    test5

--- test/Bank.t.sol
+++ test/Bank.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {Bank} from "../src/Bank.sol";
-import {SwapRouter} from "../lib/v3-periphery/contracts/SwapRouter.sol";
+import {SwapRouter} from "../src/SwapRouter.sol";
 
 contract BankTest is Test {
     Bank public bank;
